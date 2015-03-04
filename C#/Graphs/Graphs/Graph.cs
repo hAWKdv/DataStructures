37912bda6901ece0d0ffa2bf868d94caf6820067
George S. <hawk.dvp@gmail.com>
2015-03-04 18:10:45 +0200

    Some refactoring of Graph class

--- C#/Graphs/Graphs/Graph.cs
+++ C#/Graphs/Graphs/Graph.cs
@@ -21,6 +21,9 @@
             this.nodes = new HashSet<INode<T>>();
         }
 
+        /// <summary>
+        /// Returns the number of nodes in the graph
+        /// </summary>
         public int Size
         {
             get
@@ -29,6 +32,9 @@
             }
         }
 
+        /// <summary>
+        /// Sets a strategy algorithm for finding the shortest bath between two nodes.
+        /// </summary>
         public IShortestPath<T> ShortestPathStrategy
         {
             get
@@ -43,6 +49,9 @@
             }
         }
 
+        /// <summary>
+        /// Sets a strategy algorithm for traversing the graph.
+        /// </summary>
         public ITraverse<T> TraversingStrategy
         {
             get
@@ -57,6 +66,9 @@
             }
         }
 
+        /// <summary>
+        /// Sets a strategy algorithm for finding the minimal spanning tree of the graph.
+        /// </summary>
         public IMinimalSpanningTree<T> MSTStrategy
         {
             get
@@ -71,6 +83,9 @@
             }
         }
 
+        /// <summary>
+        /// Returns reference of all nodes of the graph.
+        /// </summary>
         public ISet<INode<T>> Nodes
         {
             get
@@ -79,11 +94,19 @@
             }
         }
 
+        /// <summary>
+        /// Add a single INode to the graph.
+        /// </summary>
+        /// <param name="node">Node to be added</param>
         public void AddNode(INode<T> node)
         {
             this.nodes.Add(node);
         }
 
+        /// <summary>
+        /// Add a set of INodes to the graph.
+        /// </summary>
+        /// <param name="nodes">Set of nodes to be added</param>
         public void AddNodes(params INode<T>[] nodes)
         {
             foreach (INode<T> node in nodes)
@@ -92,6 +115,11 @@
             }
         }
 
+        /// <summary>
+        /// Remove a node from the graph.
+        /// Note: All connections are destroyed.
+        /// </summary>
+        /// <param name="value">Generic value of the node</param>
         public void RemoveNode(T value)
         {
             INode<T> node = this.FindNode(value);
@@ -102,15 +130,15 @@
                 {
                     for (int i = 0; i < node.AdjacentEdges.Count; i++)
                     {
-                        var connection = node.AdjacentEdges[i];
+                        IEdge<T> adjEdge = node.AdjacentEdges[i];
 
-                        for (int j = 0; j < connection.Node.AdjacentEdges.Count; j++)
+                        for (int j = 0; j < adjEdge.Node.AdjacentEdges.Count; j++)
                         {
-                            var neighbourConnection = connection.Node.AdjacentEdges[j];
+                            IEdge<T> adjEdgeLinks = adjEdge.Node.AdjacentEdges[j];
 
-                            if (neighbourConnection.Node.Equals(node))
+                            if (adjEdgeLinks.Node.Equals(node))
                             {
-                                connection.Node.AdjacentEdges.Remove(neighbourConnection);
+                                adjEdge.Node.AdjacentEdges.Remove(adjEdgeLinks);
                             }
                         }
                     }
@@ -120,6 +148,11 @@
             }
         }
 
+        /// <summary>
+        /// Finds a node in the graph by a provided generic value.
+        /// </summary>
+        /// <param name="value">Value of the node</param>
+        /// <returns>Returns INode or null if there isn't such node</returns>
         public INode<T> FindNode(T value)
         {
             foreach (INode<T> node in this.nodes)
@@ -133,36 +166,56 @@
             return null;
         }
 
+        /// <summary>
+        /// Calculates the cost of the shorted path between the nodes.
+        /// </summary>
+        /// <param name="startNode">Start INode</param>
+        /// <param name="targetNode">Target INode</param>
+        /// <returns>Returns an integer with the total cost of the path</returns>
         public int CalculateShortestPath(INode<T> startNode, INode<T> targetNode)
         {
             if (this.ShortestPathStrategy == null)
             {
-                throw new NullAlgorithmStrategyException();
+                throw new NullAlgorithmStrategyException("The shortest path strategy of the graph is not set.");
             }
 
             return this.ShortestPathStrategy.CalculateShortestPath(startNode, targetNode);
         }
 
+        /// <summary>
+        /// Peforms a traversal of the whole graph.
+        /// </summary>
+        /// <param name="startNode">Start node</param>
+        /// <param name="manipulation">A delegate that defines the operations executed on every visited node</param>
         public void Traverse(INode<T> startNode, TraverseManipulation<T> manipulation)
         {
             if (this.TraversingStrategy == null)
             {
-                throw new NullAlgorithmStrategyException();
+                throw new NullAlgorithmStrategyException("The traversing strategy of the graph is not set.");
             }
 
             this.traversingStrategy.Traverse(startNode, manipulation);
         }
 
+        /// <summary>
+        /// Finds the minimal spanning tree of the graph.
+        /// Note: If the strategy is Kruskal, read its description.
+        /// </summary>
+        /// <returns>Returns Graph<T> tree</returns>
         public Graph<T> FindMST()
         {
             if (this.MSTStrategy == null)
             {
-                throw new NullAlgorithmStrategyException();
+                throw new NullAlgorithmStrategyException("The minimal spanning tree strategy of the graph is not set.");
             }
 
             return this.mstStrategy.FindMST();
         }
 
+        /// <summary>
+        /// Creates a deep copy/clone of the graph.
+        /// </summary>
+        /// <returns>Returns an object (Graph<T>)</returns>
         public object Clone()
         {
             Graph<T> clonedGraph = new Graph<T>();
