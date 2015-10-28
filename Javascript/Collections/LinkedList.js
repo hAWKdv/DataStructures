11a16c79754d9eeb31a7fabc077ccef1cb5f816c
hAWKdv <hawk94@abv.bg>
2015-10-29 00:24:39 +0200

    Implemented .insert() for JS Linked List

--- Javascript/Collections/LinkedList.js
+++ Javascript/Collections/LinkedList.js
@@ -2,12 +2,8 @@ var LinkedList = (function() {
     "use strict";
 
     var Item = (function() {
-        function Item(value, next) {
+        function Item(value) {
             this.value = value;
-
-            if (next) {
-                this.setNext(next);
-            }
         }
         
         Item.prototype.setNext = function (next) {
@@ -19,14 +15,44 @@ var LinkedList = (function() {
         return Item;
     }());
 
+    function _insertNewItem(value) {
+        var newItem = new Item(value);
+
+        this.tail.setNext(newItem);
+        this.tail = newItem;
+    }
+
     function LinkedList(value) {
         this.head = new Item(value);
         this.tail = this.head;
     }
 
-    LinkedList.prototype.insert = function(value) {
-        
-    };
+    LinkedList.prototype.insert = function() {
+        var self,
+            args;
 
+        if (arguments.length === 1) {
+            _insertNewItem.call(this, arguments[0]);
+        } else {
+            args = [].slice.call(arguments);
+            self = this;
+
+            args.forEach(function (argument) {
+                _insertNewItem.call(self, argument);
+            });
+        }
+
+        return this;
+    };
+    
     return LinkedList;
 }());
+
+// NOTE: Test only
+var list = new LinkedList(1);
+list.insert(2, 3, 4);
+
+console.log(list.head.value);
+console.log(list.head.next.value);
+console.log(list.head.next.next.value);
+console.log(list.head.next.next.next.value);
