16340b95c094d51e0bfbaaac0de0962fc1ff4f68
hawkgs <hawk.dvp@gmail.com>
2019-08-30 11:43:02 +0300

    Fix TS heap extraction and extends Comparer

--- typescript/trees/heap.ts
+++ typescript/trees/heap.ts
@@ -1,5 +1,21 @@
-export class Heap<T> {
+import { NumberComparer, CompareSym as CompareSym, CompareFunction } from '../common';
+
+export class Heap<T> extends NumberComparer<T> {
   private _arr: T[] = [];
+
+  constructor(initValue?: T, compare?: CompareFunction<T>) {
+    super();
+    if (initValue !== undefined) {
+      this._arr.push(initValue);
+    }
+    if (compare) {
+      this.compare = compare;
+    }
+  }
+
+  get empty(): boolean {
+    return this._arr.length === 0;
+  }
 
   insert(value: T) {
     this._arr.push(value);
@@ -10,9 +26,13 @@ export class Heap<T> {
     const first = this._arr[0];
 
     if (first) {
-      const last = this._arr.pop();
-      this._arr[0] = last;
-      this._balanceExtract(0);
+      if (this._arr.length > 1) {
+        const last = this._arr.pop();
+        this._arr[0] = last;
+        this._balanceExtract(0);
+      } else {
+        this._arr = [];
+      }
     }
 
     return first;
@@ -22,7 +42,7 @@ export class Heap<T> {
     let child = this._arr.length - 1;
     let parent = this._getParentIdx(child);
 
-    while (this._arr[parent] > this._arr[child]) {
+    while (this.compare(this._arr[parent], CompareSym.GT, this._arr[child])) {
       const swap = this._arr[parent];
       this._arr[parent] = this._arr[child];
       this._arr[child] = swap;
@@ -37,10 +57,10 @@ export class Heap<T> {
     let leftChild = this._getLeftIdx(idx);
     let rightChild = this._getRightIdx(idx);
 
-    if (this._arr[idx] > this._arr[leftChild]) {
+    if (this.compare(this._arr[idx], CompareSym.GT, this._arr[leftChild])) {
       smallest = leftChild;
     }
-    if (this._arr[idx] > this._arr[rightChild]) {
+    if (this.compare(this._arr[idx], CompareSym.GT, this._arr[rightChild])) {
       smallest = rightChild;
     }
     if (smallest !== idx) {
